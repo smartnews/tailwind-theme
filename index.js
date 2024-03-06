aab027188b59b7f0ccbf6afaf36fadcfee5043b4
Michelle Lei <michellerp1013@gmail.com>
2024-03-06 11:31:46 +0900

    feat: define gray colors

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const plugin = require("tailwindcss/plugin");
-
-const colCount = plugin(
-  function ({ addUtilities, theme, e }) {
-    const values = theme("colCount");
-
-    var utilities = Object.entries(values).map(([key, value]) => {
-      return {
-        [`.${e(`col-count-${key}`)}`]: { columnCount: `${value}` },
-      };
-    });
-
-    addUtilities(utilities);
-  },
-  {
-    theme: {
-      colCount: {
-        2: "2",
-        3: "3",
-        4: "4",
-        5: "5",
-        6: "6",
-      },
-    },
+const themes = {
+  colors: {
+    gray: {
+      900: '#1c1c1c',
+      800: '#2b2b2b',
+      700: '#4c4c4c',
+      600: '#606060',
+      500: '#8e8e8e',
+      400: '#bababa',
+      300: '#e0e0e0',
+      200: '#ececec',
+      100: '#f9f9f9',
+      0: '#ffffff',
+    }
   }
-);
+}
 
-module.exports = colCount;
+module.exports = themes;
