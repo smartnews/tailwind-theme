9bffebd875e0cb9bfb9a76c232a038d26daad3dd
Michelle Lei <michellerp1013@gmail.com>
2024-04-19 16:15:26 +0900

    feat: add type

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const themes = {
-  colors: {
-    gray: {
-      900: '#1c1c1c',
-      800: '#2b2b2b',
-      700: '#4c4c4c',
-      600: '#606060',
-      500: '#8e8e8e',
-      400: '#bababa',
-      300: '#e0e0e0',
-      200: '#ececec',
-      100: '#f9f9f9',
-      0: '#ffffff',
-    }
-  }
-}
-
-module.exports = themes;
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          900: "#1c1c1c",
+          800: "#2b2b2b",
+          700: "#4c4c4c",
+          600: "#606060",
+          500: "#8e8e8e",
+          400: "#bababa",
+          300: "#e0e0e0",
+          200: "#ececec",
+          100: "#f9f9f9",
+          0: "#ffffff",
+        },
+      },
+    },
+  },
+};
