6fdad1765ef1f387afafb6ac91089cc393941f4a
Michelle Lei <michellerp1013@gmail.com>
2024-04-19 17:01:07 +0900

    feat: add example html

---
+++ examples/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  presets: [require("../index.js")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
